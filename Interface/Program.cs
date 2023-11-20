3fcf7fd43a025e33467b7998ad2fb6254b093460
Daniel Hartwich <dhartwich650@gmail.com>
2023-11-20 18:12:23 +0100

    InterFace-Done
    Done H2

---
+++ Interface/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Progam
+{
+    Car car = new Car(); 
+         Banana banana = new Banana();
+         Person person = new Person();
+    public void Main(string[] args)
+    {
+        insert();
+    }
+
+    public void insert()
+    {
+
+        List<string> list = new List<string>()
+        {
+           list.Add(car.SigNavn)
+        };
+    }
+}
+
+public class Car
+{
+    
+    public string SigNavn(string CarName ="Volvo")
+    {
+        return CarName;
+    }
+}
+
+public class Banana
+{
+    public string SigNavn(string BananaName = "BananManden")
+    {
+        return BananaName;
+    }
+}
+
+public class Person
+{
+  public void SigNavn(string PersonName = "Palle")
+    {
+       Console.WriteLine(PersonName);
+    }
+
+}
